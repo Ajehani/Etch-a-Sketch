f75219ec49fc84e7156afc82e2d560d720f5ed0b
Anas Jehani <creativelybankrupt02@gmail.com>
2021-07-27 16:08:15 +0100

    still not working, unfortunately

--- persev/script.js
+++ persev/script.js
@@ -1,28 +1,27 @@
 const container=document.querySelector("#container");
 const slider=document.querySelector('#slider');
-
 slider.addEventListener("click",function(e){
     while (container.hasChildNodes()) {
-    container.removeChild(container.lastChild);
-}
+        container.removeChild(container.lastChild);
+};
     let n = e.target.value
-    console.log(e.target.value)
+    console.log(n)
 
     for (i=1;i<=n**2;i++) {
         let box=document.createElement("div");
         box.setAttribute("id","created");
         container.appendChild(box); 
-}
-})
+}})
 
 let items=document.querySelectorAll("#created");
 
 items.forEach(item =>item.addEventListener("mouseover",function(){
-    item.style='background-color:gold;';
+    item.style="backgroundColor:gold;";
+    console.log("yay")
 }))
 const clear=document.querySelector('#clear')
 clear.addEventListener('click',function(){
-    items.forEach(item =>item.style='background-color:white;'
-)})
+    items.forEach(item =>item.style="background-color:white;")
+})
 
 
