27a06e58e0c9af189f721a5d0aff5ce79086d7a0
Anas Jehani <creativelybankrupt02@gmail.com>
2021-12-10 22:10:20 +0000

    Happy its done in 2021

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 const container=document.querySelector("#container");
 document.querySelector('#clear').addEventListener('click',()=>document.querySelectorAll('.created').forEach(el=>el.setAttribute('style','background-color:#ededed;')));
-const omg=(n,color)=>{
-    if(n>=0 && n<=240){
+const etch=(n,color)=>{
+    if(n>=34 && n<=240){
         for(let i=1;i<=n**2;i++) {
             let box=document.createElement("div");
             box.addEventListener("mouseover",()=>{
@@ -13,12 +13,12 @@ const omg=(n,color)=>{
     } 
 }
 document.querySelector('#click').addEventListener("click",()=>{
-    let n=Number(prompt("Enter Size(between 1 and 240): "));
+    let num=Number(prompt("Enter Size(between 24 and 240): "));
     let c=document.querySelector('#color').value
     while (container.hasChildNodes()) {
         container.removeChild(container.lastChild);
     }
-    omg(n,c);
+    etch(num,c);
 });
 
 
